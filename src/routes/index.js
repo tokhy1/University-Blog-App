757257c88fb34ec29d310fc1bf667b5e6d468383
tokhy24 <mohamed21346587@gmail.com>
2026-05-01 16:29:29 +0300

    chore: remove blog application
    - Initial Setup for the project.

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { PostController } from "../controllers/postController.js";
-
-const router = express.Router();
-
-// GET / → Home page with optional filters
-router.get("/", PostController.home);
-
-export default router;
